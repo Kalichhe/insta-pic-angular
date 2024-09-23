f5d0493cdda91e5997631e3468475a323af1c843
Kalichhe <jhondh2003@gmail.com>
2024-09-23 00:11:08 -0500

    Cosas

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { User } from '../../interfaces/user.interface';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+    user:User = {
+        userName: '',
+        password: ''
+    };
+
+    loginForm = this.fb.group({
+        userName:[''],
+        password:['']
+    });
+
+    constructor(private fb:FormBuilder, private router:Router) { }
+
+    onLogin(){
+
+        let userName = this.loginForm.value.userName;
+        let password = this.loginForm.value.password;
+
+        if (!userName || !password) {
+            alert('Debe diligenciar los campos');
+            return;
+        }
+        const storedPassword = localStorage.getItem(userName.toLowerCase());
+
+        if (storedPassword === null) {
+            alert('Usuario no registrado');
+        } else if (storedPassword === password) {
+            alert('Inicio de sesión exitoso');
+            this.router.navigateByUrl('/home');
+        } else {
+            alert('Contraseña incorrecta');
+        }
+    }
 
 }
--- src/app/auth/pages/sign-up/sign-up.component.ts
+++ src/app/auth/pages/sign-up/sign-up.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+
+    signUpForm = this.fb.group({
+        userName:['', [Validators.required]],
+        email:['', [Validators.required]],
+        password:['', [Validators.required]],
+        reTypePassword:['', [Validators.required]]
+    });
+
+    userService = inject(UserService);
+
+    constructor(private fb:FormBuilder, private router:Router) { }
+
+    onRegister(){
+        if(!this.signUpForm.valid){
+            Swal.fire({
+                title: "Registrar usuario",
+                text: "Diligencie todos los campos",
+                icon: "error"
+              });
+            return;
+        }
+
+        const userName = this.signUpForm.value.userName;
+        const password = this.signUpForm.value.password;
+        const reTypePassword = this.signUpForm.value.reTypePassword;
+
+        if(password !== reTypePassword){
+            alert('Las contraseñas no coinciden');
+            return;
+        }
+
+        if (localStorage.getItem(userName!.trim().toLowerCase())) {
+            alert('El usuario ya existe');
+            return;
+        }
+
+        localStorage.setItem(userName!.trim().toLowerCase(), password!);
+        this.router.navigateByUrl('/home');
+    }
 
 }
